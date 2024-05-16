aded317a21a2d9441139c3797684a2368374e20c
dmckeow <deanandrewmckeown@gmail.com>
2024-05-16 11:31:53 -0500

    Update spillover-SNP.sh

--- bin/spillover-SNP.sh
+++ bin/spillover-SNP.sh
@@ -9,9 +9,9 @@ BAMS="/panfs/jay/groups/27/dcschroe/dmckeow/data/Spillover_FINAL/tmp.mapping.sel
 
 ## model is dna_r10.4.1_e8.2_400bps_hac@v4.2.0
 for f in $(ls $BAMS/*.bam); do 
-    HDF=$(ls SNP/$(basename $f .bam).hdf)
-	FA=$(ls tmp.derep/$(basename $f .bam).contigs.fasta)
-	VCF=$(ls SNP/$(basename $f .bam).vcf)
+    HDF=$(echo SNP/$(basename $f .bam).hdf)
+	FA=$(echo tmp.derep/$(basename $f .bam).contigs.fasta)
+	VCF=$(echo SNP/$(basename $f .bam).vcf)
 
 	medaka consensus $f $HDF --threads $SLURM_CPUS_PER_TASK --model r1041_e82_400bps_hac_v4.2.0
 
